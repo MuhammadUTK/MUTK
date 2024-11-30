456299ba25253cd5c92a3098882cc7019a1349d8
MuhammadUTK <usman.tariq7134@gmail.com>
2024-11-30 23:36:43 +0500

    implement image orbits

--- src/component/Resume/ResumeBody/HeroSection.jsx
+++ src/component/Resume/ResumeBody/HeroSection.jsx
@@ -1,5 +1,9 @@
 import "./HeroSection.css"
 import HeroImage from "../Images/Astronaut.png"
+import Www from "../Images/www.png"
+import Mail from "../Images/mail.png"
+import Smartphone from "../Images/smartphone.png"
+
 
 function HeroSection() {
     return (
@@ -9,8 +13,20 @@ function HeroSection() {
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum error illum in laudantium maxime mollitia numquam praesentium quasi soluta, temporibus. A accusantium alias atque aut consequuntur distinctio doloribus illum, officia quisquam tempora temporibus vero. Accusamus eius hic perferendis perspiciatis rerum, similique? At dolor eligendi placeat saepe. Assumenda, beatae blanditiis cumque exercitationem facilis, labore nemo omnis quam, sit tempora voluptate voluptatem.
                 </div>
                 <div className="resume-hero-graphics">
-                    <div className="resume-image">
-                        <img src={HeroImage} alt="astonaut"/>
+                    <div className="orbit-container">
+                        <div className="resume-image">
+                            <img src={HeroImage} alt="astonaut"/>
+                        </div>
+                        <div className="orbit orbit-1">
+                            <img src={Www} alt="" className="orbit-img"/>
+                            <img src={Smartphone} alt="" className="orbit-img"/>
+                            <img src={Mail} alt="" className="orbit-img"/>
+                        </div>
+                        <div className="orbit orbit-2">
+                            <img src={Www} alt="" className="orbit-img"/>
+                            <img src={Smartphone} alt="" className="orbit-img"/>
+                            <img src={Mail} alt="" className="orbit-img"/>
+                        </div>
                     </div>
                     <div className="resume-hero-cloud">
                         <div className="resume-hero-cloud-circle big-1"></div>
