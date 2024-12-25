b57238db0fc723fbaceb0dc248ccc81a68947396
MuhammadUTK <usman.tariq7134@gmail.com>
2024-12-25 20:47:36 +0500

    start updating hero section

--- src/component/Resume/HeroSection/HeroSection.jsx
+++ src/component/Resume/HeroSection/HeroSection.jsx
@@ -6,6 +6,9 @@ import Currency from "../Images/Orbit/currency.png"
 import Layout from "../Images/Orbit/layout.png"
 import Social from "../Images/Orbit/social.png"
 import Smartphone from "../Images/Orbit/smartphone.png"
+import StarRating from "../Images/Hero-Animation/star-rating.png"
+import DiamondBullet from "../Images/Hero-Animation/diamond-bullet.png"
+import Customer1 from "../Images/Convincing/layout1.png"
 
 
 function HeroSection() {
@@ -58,35 +61,80 @@ function HeroSection() {
                     </div>
                 </div>
                 <div className="resume-hero-graphics">
-                    <div className="orbit-container">
-                        <div className="resume-image">
-                            <img src={HeroImage} alt="astonaut"/>
+                    <div className="hero-animation-container">
+                        <div className="grid-item project-details">
+                            <div className="project-title">
+                                <h3>About Project</h3>
+                            </div>
+                            <div className="project-data">
+                                <div className="project-description">
+                                    <p><b className="website-name">NFT Galaxy</b> is a cutting-edge platform where artists, collectors, and enthusiasts converge to buy, sell, and trade unique digital assets.</p>
+                                </div>
+                                <div className="project-bullet-facts">
+                                    <div className="bullet-facts">
+                                        <img src={DiamondBullet} alt=""/>
+                                        <p className="fact-title">&nbsp;&nbsp;Launched&nbsp;</p>
+                                        <p>March 2023</p>
+                                    </div>
+                                    <div className="bullet-facts">
+                                        <img src={DiamondBullet} alt=""/>
+                                        <p className="fact-title">&nbsp;&nbsp;Transactions&nbsp;</p>
+                                        <p>Over $10 Million in Sales</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="orbit orbit-1">
-                            <img src={Analytics} alt="" className="orbit-img"/>
-                            <img src={Smartphone} alt="" className="orbit-img"/>
-                            <img src={Cloud} alt="" className="orbit-img"/>
+                        <div className="grid-item client-description">
+                            <div className="client-name-title">
+                                <h3>Satisfied Customer</h3>
+                            </div>
+                            <div className="client-details">
+                                <div className="client-name">
+                                    <p>Mathew Doe</p>
+                                </div>
+                                <div className="client-rating">
+                                    <p>Rating</p>
+                                    <img src={StarRating} alt="Client Rating"/>
+                                </div>
+                            </div>
                         </div>
-                        <div className="orbit orbit-2">
-                            <img src={Social} alt="" className="orbit-img"/>
-                            <img src={Currency} alt="" className="orbit-img"/>
-                            <img src={Layout} alt="" className="orbit-img"/>
+                        <div className="grid-item client-image">
+                            <img src={Customer1} alt=""/>
+                        </div>
+                        <div className="grid-item project-layout">
                         </div>
                     </div>
-                    <div className="resume-hero-cloud">
-                        <div className="resume-hero-cloud-circle big-1"></div>
-                        <div className="resume-hero-cloud-circle big-2"></div>
-                        <div className="resume-hero-cloud-circle medium-1"></div>
-                        <div className="resume-hero-cloud-circle medium-2"></div>
-                        <div className="resume-hero-cloud-circle medium-3"></div>
-                        <div className="resume-hero-cloud-circle medium-4"></div>
-                        <div className="resume-hero-cloud-circle medium-5"></div>
-                        <div className="resume-hero-cloud-circle medium-6"></div>
-                        <div className="resume-hero-cloud-circle small-1"></div>
-                        <div className="resume-hero-cloud-circle small-2"></div>
-                        <div className="resume-hero-cloud-circle small-3"></div>
-                        <div className="resume-hero-cloud-circle small-4"></div>
-                    </div>
+
+
+                    {/*<div className="orbit-container">*/}
+                    {/*    <div className="resume-image">*/}
+                    {/*        <img src={HeroImage} alt="astonaut"/>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="orbit orbit-1">*/}
+                    {/*        <img src={Analytics} alt="" className="orbit-img"/>*/}
+                    {/*        <img src={Smartphone} alt="" className="orbit-img"/>*/}
+                    {/*        <img src={Cloud} alt="" className="orbit-img"/>*/}
+                    {/*    </div>*/}
+                    {/*    <div className="orbit orbit-2">*/}
+                    {/*        <img src={Social} alt="" className="orbit-img"/>*/}
+                    {/*        <img src={Currency} alt="" className="orbit-img"/>*/}
+                    {/*        <img src={Layout} alt="" className="orbit-img"/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    {/*<div className="resume-hero-cloud">*/}
+                    {/*    <div className="resume-hero-cloud-circle big-1"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle big-2"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-1"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-2"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-3"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-4"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-5"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle medium-6"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle small-1"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle small-2"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle small-3"></div>*/}
+                    {/*    <div className="resume-hero-cloud-circle small-4"></div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
