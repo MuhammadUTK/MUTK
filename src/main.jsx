e8b1ba8c8b25d96cc987082cf5177b07b0afd049
MuhammadUTK <usman.tariq7134@gmail.com>
2025-01-27 15:12:13 +0500

    Add Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
